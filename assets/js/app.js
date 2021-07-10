60ab2f02f8d51335724b4cb717b5aa5fa7d3a990
Casey Juergens <79878212+caseyjuergens@users.noreply.github.com>
2021-07-09 21:51:21 -0700

    reading in data, setting up svg wrapper

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,27 @@
-// @TODO: YOUR CODE HERE!
+
+//create a scatter plot that represents each state with circle elements
+
+//read in data from data.csv
+//include state abbreviation sin circles
+//create and situate your axes and labels to the left & bottom of chart
+
+//set up svg dimensions
+var svgWidth=960;
+var svgHeight=500;
+var margin= {top: 20, right: 20, bottom: 20, left:20};
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+//svg
+var svg = d3.select(".scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth)
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//import data
+d3.csv("data.csv").then(function(data) {
+    data.forEach(function(data))
+
+});
